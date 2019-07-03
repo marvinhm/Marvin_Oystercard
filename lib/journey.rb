7c55fa76134c02180ab535d697e50123e6e3149c
marvinmatovu@gmail.com <student@Makerss-MacBook-Pro.local>
2019-07-03 17:12:43 +0100

    step 14 - finished

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,35 +1,45 @@
+# frozen_string_literal: true
+
 # require 'Pry'
 
 class Journey
+  attr_reader :in_journey, :entry_station, :exit_station, :all_journeys, :one_journey
+  MINIMUM_FARE = 1
+  PENALTY_FARE = 6
 
-   attr_reader :in_journey, :entry_station, :exit_station, :all_journeys, :one_journey
+  def initialize
+    @in_journey = false
+    @entry_station = nil
+    @exit_station = nil
+    @all_journeys = []
+    @one_journey = { @entry_station => @exit_station }
+  end
 
+  def touch_in(station)
+    raise('You have already tapped in!') if @in_journey == true
 
-    def intialize 
-        p "helloooooo"
-        @in_journey = false
-        @entry_station = nil
-        @exit_station = nil
-        @all_journeys = []
-        @one_journey = {@entry_station => @exit_station}
-    end 
+    @in_journey = true
+    @one_journey = { station => @exit_station }
+    @entry_station = station
+  end
 
-    def touch_in(station)
-        @entry_station = station
-        @one_journey = {station => @exit_station}
-        @entry_station = true 
+  def touch_out(station)
+    raise('You have already tapped out!') if @in_journey == false
+
+    @exit_station = station
+    @one_journey[@entry_station] = station
+    @all_journeys.push(@one_journey)
+    @in_journey = false
+    # @entry_station = nil
+  end
+
+  def fare
+    if entry_station == nil || exit_station == nil
+      return PENALTY_FARE
+    else
+      return MINIMUM_FARE
     end
-    
-      def touch_out(station)
-        @exit_station = station
-        p "Hellllooooooooooooo"
-        @one_journey[@entry_station] = station
-        p "Hellllooooooooooooo"
-        @all_journeys
-        @all_journeys.push(@one_journey)
-        @in_journey = false
-        # @entry_station = nil
-      end
+  end
 end
 
 # binding.pry
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,41 +1,78 @@
+# frozen_string_literal: true
+
 require 'journey'
 
 describe Journey do
-  let(:journey){ double :journey }
-  let(:station){ double :station }
-  let(:station_name){ double :station_name }
+  let(:journey) { double :journey }
+  let(:station) { double :station }
+  let(:station_name) { double :station_name }
   # let(:journey){ {entry_station: entry_station, exit_station: exit_station} }
 
   describe '#touch_in' do
-  it "Commences a journey" do
-    # subject.top_up 1
-    expect(journey.touch_in("Kings Cross")).to eq journey.in_journey
+    it 'Commences a journey' do
+      # subject.top_up 1
+      allow(journey).to receive(:touch_in).and_return true
+      expect(journey.touch_in(station_name)).to eq true
+    end
+
+    it 'raises an error if user has already tapped in' do
+      subject.touch_in(station_name)
+      expect{subject.touch_in(station_name)}.to raise_error("You have already tapped in!")
+    end
   end
-end 
 
   it "allows a user to know where they've travelled from" do
     # subject.top_up 5
-    allow(journey).to receive(:touch_in).and_return true 
+    allow(journey).to receive(:touch_in).and_return true
     # journey.touch_in("Kings Cross")
     expect(journey.touch_in(station_name)).to eq true
   end
 
   describe '#touch_out' do
-    it "Ends a journey" do
+    it 'Ends a journey' do
       # subject.top_up 5
-      journey.touch_in("Kings Cross")
-      journey.touch_out station
+      allow(journey).to receive(:touch_in).and_return true
+      allow(journey).to receive(:touch_out).and_return false
+      allow(journey).to receive(:in_journey).and_return false
+      journey.touch_in(station_name)
+      journey.touch_out station_name
       expect(journey.in_journey).to eq false
     end
 
-  it 'has an empty list of journeys by default' do
-    expect(journey.all_journeys).to be_empty
+    it 'has an empty list of journeys by default' do
+      allow(journey).to receive(:all_journeys).and_return []
+      expect(journey.all_journeys).to be_empty
+    end
+
+    it 'raises an error if user has already tapped out' do
+      subject.touch_in(station_name)
+      subject.touch_out(station_name)
+      expect{subject.touch_out(station_name)}.to raise_error("You have already tapped out!")
+    end
+
+    # it 'stores a journey' do
+    #   journey.touch_in(entry_station)
+    #   journey.touch_out(exit_station)
+    #   expect(journey.all_journeys).to include one_journey
+    # end
   end
 
-  # it 'stores a journey' do
-  #   journey.touch_in(entry_station)
-  #   journey.touch_out(exit_station)
-  #   expect(journey.all_journeys).to include one_journey
-  # end
- end 
+  describe '#fare' do
+    it 'Should return minimum fare or penalty fare' do
+      subject.touch_in(station_name)
+      expect(subject.entry_station).not_to be(nil)
+    end
+
+    it 'Should return minimum fare or penalty fare' do
+      subject.touch_in(station_name)
+      subject.touch_out(station_name)
+      expect(subject.exit_station).not_to be(nil)
+    end
+
+    it 'Should return minimum fare' do
+      subject.touch_in(station_name)
+      subject.touch_out(station_name)
+      expect(subject.fare).to eq(Journey::MINIMUM_FARE)
+    end
+  end
 end
