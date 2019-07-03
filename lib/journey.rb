d53b9362ed4a2d892de3d1f8c61ff33ba8af472e
Student <student@Makerss-MacBook-Pro-2.local>
2019-07-03 16:18:43 +0100

    working on step 14

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,5 +1,35 @@
-
+# require 'Pry'
 
 class Journey
 
+   attr_reader :in_journey, :entry_station, :exit_station, :all_journeys, :one_journey
+
+
+    def intialize 
+        p "helloooooo"
+        @in_journey = false
+        @entry_station = nil
+        @exit_station = nil
+        @all_journeys = []
+        @one_journey = {@entry_station => @exit_station}
+    end 
+
+    def touch_in(station)
+        @entry_station = station
+        @one_journey = {station => @exit_station}
+        @entry_station = true 
+    end
+    
+      def touch_out(station)
+        @exit_station = station
+        p "Hellllooooooooooooo"
+        @one_journey[@entry_station] = station
+        p "Hellllooooooooooooo"
+        @all_journeys
+        @all_journeys.push(@one_journey)
+        @in_journey = false
+        # @entry_station = nil
+      end
 end
+
+# binding.pry
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,9 +1,41 @@
 require 'journey'
 
 describe Journey do
-  describe '#record' do
-    it 'Should record a journey' do
-      
-    end
+  let(:journey){ double :journey }
+  let(:station){ double :station }
+  let(:station_name){ double :station_name }
+  # let(:journey){ {entry_station: entry_station, exit_station: exit_station} }
+
+  describe '#touch_in' do
+  it "Commences a journey" do
+    # subject.top_up 1
+    expect(journey.touch_in("Kings Cross")).to eq journey.in_journey
   end
+end 
+
+  it "allows a user to know where they've travelled from" do
+    # subject.top_up 5
+    allow(journey).to receive(:touch_in).and_return true 
+    # journey.touch_in("Kings Cross")
+    expect(journey.touch_in(station_name)).to eq true
+  end
+
+  describe '#touch_out' do
+    it "Ends a journey" do
+      # subject.top_up 5
+      journey.touch_in("Kings Cross")
+      journey.touch_out station
+      expect(journey.in_journey).to eq false
+    end
+
+  it 'has an empty list of journeys by default' do
+    expect(journey.all_journeys).to be_empty
+  end
+
+  # it 'stores a journey' do
+  #   journey.touch_in(entry_station)
+  #   journey.touch_out(exit_station)
+  #   expect(journey.all_journeys).to include one_journey
+  # end
+ end 
 end
